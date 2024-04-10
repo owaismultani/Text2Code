e20f529f150594c923289e55abb6f957b756376c
Owais Multani <owais@quantive.com>
2024-04-10 15:55:31 -0400

    :sparkles: v6

--- text2code/settings.py
+++ text2code/settings.py
@@ -149,5 +149,3 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 # ENVIRONMENT VARIABLES
 import os
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-if not OPENAI_API_KEY:
-    raise ValueError('OPENAI_API_KEY environment variable not set')
