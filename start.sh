0793652c702b2941bb18a1ceb1ab3d23ea3c41ca
Owais Multani <owais@quantive.com>
2024-04-11 20:27:23 -0400

    :sparkles: updated start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@
 
 # Django setting
 export DJANGO_SETTINGS_MODULE='text2code.settings.production'
-
+poetry run python manage.py collectstatic --noinput
+poetry run python manage.py migrate
 poetry run python -m uvicorn text2code.asgi:application --host 0.0.0.0 --port 8000
